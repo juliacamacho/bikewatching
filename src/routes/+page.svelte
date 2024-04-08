adad15d95d9173b17e76b670a5f322e718b35d52
Julia Camacho <juliac123@gmail.com>
2024-04-08 12:06:23 -0400

    heading

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Bikewatching</h1>
+<p>This page displays Boston's bike traffic throughout the day.</p>
