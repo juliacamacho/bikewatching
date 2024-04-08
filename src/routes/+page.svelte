ffd879b368af51e4467b321f43ed829c9ef4244c
Julia Camacho <juliac123@gmail.com>
2024-04-08 12:29:33 -0400

    add map

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,25 @@
+<script>
+    import mapboxgl from "mapbox-gl";
+    import "../../node_modules/mapbox-gl/dist/mapbox-gl.css";
+    mapboxgl.accessToken = "pk.eyJ1IjoianVsaWFjYyIsImEiOiJjbHVyNWo0aDcwM2p4MmlvM2RmNDN3NnA3In0.akewVVgfBwvektMVj8KnfQ";
+
+    import { onMount } from "svelte";
+
+    onMount(() => {
+        let map = new mapboxgl.Map({
+            container: "map",
+            style: "mapbox://styles/mapbox/streets-v12",
+            center: [-71.09415, 42.36027],
+            zoom: 12
+        });
+    })
+
+</script>
+
 <h1>Bikewatching</h1>
 <p>This page displays Boston's bike traffic throughout the day.</p>
+<div id="map" />
+
+<style>
+    @import url("$lib/global.css");
+</style>
